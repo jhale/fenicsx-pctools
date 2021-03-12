0d97bc399b35d76307b5c9bea09d3312bd47ccca
Martin Řehoř <martin.rehor@rafinex.com>
2021-03-12 11:18:45 +0100

    Allow docker image builds based on an image name passed to build script as the 1st argument.

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -eo pipefail
-tag=$1
-[ $# -eq 0 ] && { echo "Usage: $0 <tag>"; exit 1; }
+image=$1
+target=$2
+tag=${3:-$target}
+[ $# -le 1 ] && { echo "Usage: $0 <tag> <target> [tag (default: same as target)]"; exit 1; }
 
 set -u
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker build \
     -t local/fenics_pctools:${tag} \
     -f ${script_dir}/Dockerfile \
-    --target ${tag} \
+    --target ${target} \
+    --build-arg IMAGE=${image} \
     ${script_dir}/..
