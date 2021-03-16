aa69283bc1b17e7250ffb5953728a394f06b9ba6
Jack S. Hale <mail@jackhale.co.uk>
2021-03-16 12:32:19 +0100

    Forms in, needs testing.

--- examples/cylinder/sandbox/test_bdm_interpolation.py
+++ examples/cylinder/sandbox/test_bdm_interpolation.py
@@ -4,7 +4,7 @@ from mpi4py import MPI
 import dolfinx
 import ufl
 
-mesh = dolfinx.UnitSquareMesh(MPI.COMM_WORLD, 10, 10)
+mesh = dolfinx.UnitSquareMesh(MPI.COMM_WORLD, 1, 1)
 
 V_el = ufl.VectorElement("CG", ufl.triangle, 2)
 V = dolfinx.FunctionSpace(mesh, V_el) 
@@ -14,7 +14,19 @@ U = dolfinx.FunctionSpace(mesh, U_el)
 
 v = dolfinx.Function(V)
 
-v_p = ufl.TrialFunction(V)
-w_p = ufl.TestFunction(V)
+v_p = ufl.TrialFunction(U)
+w_p = ufl.TestFunction(U)
 
-a = inner(
+n = ufl.FacetNormal(mesh)
+
+dS = ufl.Measure("dS", mesh)
+ds = ufl.Measure("ds", mesh)
+a = ufl.inner(ufl.inner(v_p("+"), n("+")), ufl.inner(w_p("+"), n("+")))*dS + ufl.inner(ufl.inner(v_p, n), ufl.inner(w_p, n))*ds
+L = ufl.inner(ufl.inner(v("+"), n("+")), ufl.inner(w_p("+"), n("+")))*dS + ufl.inner(ufl.inner(v, n), ufl.inner(w_p, n))*ds
+
+A = dolfinx.fem.assemble_matrix(a)
+A.assemble()
+print(A.convert('dense').getDenseArray())
+
+b = dolfinx.fem.assemble_vector(L)
+print(b.array)
