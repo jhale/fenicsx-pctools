4f9135273a71c251e02d7eacce48f14f2a60bcb3
Jack S. Hale <mail@jackhale.co.uk>
2021-03-16 15:47:05 +0100

    Solve system.

--- examples/cylinder/sandbox/test_bdm_interpolation.py
+++ examples/cylinder/sandbox/test_bdm_interpolation.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 from mpi4py import MPI
+from petsc4py import PETSc
 
 import dolfinx
 import ufl
+from ufl import inner, dot
 
-mesh = dolfinx.UnitSquareMesh(MPI.COMM_WORLD, 1, 1)
+mesh = dolfinx.UnitSquareMesh(MPI.COMM_WORLD, 64, 64)
 
 V_el = ufl.VectorElement("CG", ufl.triangle, 2)
 V = dolfinx.FunctionSpace(mesh, V_el) 
@@ -12,21 +14,43 @@ V = dolfinx.FunctionSpace(mesh, V_el)
 U_el = ufl.FiniteElement("BDM", ufl.triangle, 1)
 U = dolfinx.FunctionSpace(mesh, U_el)
 
+def data(x):
+    return (x[0], x[1])
+
 v = dolfinx.Function(V)
+v.interpolate(data)
 
 v_p = ufl.TrialFunction(U)
 w_p = ufl.TestFunction(U)
 
 n = ufl.FacetNormal(mesh)
 
-dS = ufl.Measure("dS", mesh)
-ds = ufl.Measure("ds", mesh)
-a = ufl.inner(ufl.inner(v_p("+"), n("+")), ufl.inner(w_p("+"), n("+")))*dS + ufl.inner(ufl.inner(v_p, n), ufl.inner(w_p, n))*ds
-L = ufl.inner(ufl.inner(v("+"), n("+")), ufl.inner(w_p("+"), n("+")))*dS + ufl.inner(ufl.inner(v, n), ufl.inner(w_p, n))*ds
+dS = ufl.Measure("dS", mesh, metadata={"quadrature_degree" : 1})
+ds = ufl.Measure("ds", mesh, metadata={"quadrature_degree" : 1})
+a = inner(dot(v_p("+"), n("+")), dot(w_p("+"), n("+")))*dS + inner(dot(v_p, n), dot(w_p, n))*ds
+L = inner(dot(v("+"), n("+")), dot(w_p("+"), n("+")))*dS + inner(dot(v, n), dot(w_p, n))*ds
 
 A = dolfinx.fem.assemble_matrix(a)
 A.assemble()
-print(A.convert('dense').getDenseArray())
 
 b = dolfinx.fem.assemble_vector(L)
-print(b.array)
+b.ghostUpdate(addv=PETSc.InsertMode.ADD, mode=PETSc.ScatterMode.REVERSE)
+
+PETSc.Options().setValue("ksp_view", "")
+PETSc.Options().setValue("ksp_monitor_true_residual", "")
+solver = PETSc.KSP().create(MPI.COMM_WORLD)
+solver.setOperators(A)
+# Slow
+#solver.setType("preonly")
+#solver.getPC().setType("lu")
+#solver.getPC().setFactorSolverType("mumps")
+# Seems to work well
+#solver.setType("richardson")
+#solver.getPC().setType("jacobi")
+# Also seems to work well.
+solver.setType("cg")
+solver.getPC().setType("jacobi")
+solver.setFromOptions()
+
+v_p_h = dolfinx.Function(U)
+solver.solve(b, v_p_h.vector)
