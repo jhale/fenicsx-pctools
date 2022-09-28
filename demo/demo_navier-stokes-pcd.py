d276c38a64e3b1d2718b35c8ec515152437ad583
Jack S. Hale <mail@jackhale.co.uk>
2022-09-28 09:21:35 +0200

    Attempt at documentation build

---
+++ demo/demo_navier-stokes-pcd.py
@@ -0,0 +1,11 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.1
+# ---
+
+# # Navier-Stokes equations with a PCD-based preconditioner 
