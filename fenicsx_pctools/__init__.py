c5db74fe0b776f3c4323d0a2c7d950b78f2ee9c4
Martin Řehoř <martin.rehor@rafinex.com>
2022-10-03 21:10:44 +0200

    Clean up init file in root dir.

--- fenicsx_pctools/__init__.py
+++ fenicsx_pctools/__init__.py
@@ -2,16 +2,14 @@ __version__ = "2021.1.0.dev0"
 
 from fenicsx_pctools import mat, pc
 
-from .mat.splittable import create_splittable_matrix_block, create_splittable_matrix_monolithic
+# Imports allowing shorthand specification of preconditioners in PETSc config files
 from .pc.pcd import PCDPC_vX, PCDPC_vY
 from .pc.wrapped import WrappedPC
 
 __all__ = [
-    "create_splittable_matrix_block",
-    "create_splittable_matrix_monolithic",
+    "mat",
+    "pc",
     "WrappedPC",
     "PCDPC_vX",
     "PCDPC_vY",
-    "mat",
-    "pc",
 ]
