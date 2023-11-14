7491639b3f919e416e498adcd6d0bc38545cd291
Martin Řehoř <martin.rehor@rafinex.com>
2023-11-14 10:34:54 +0100

    Add SPDX headers to src files.

--- fenicsx_pctools/__init__.py
+++ fenicsx_pctools/__init__.py
@@ -1,3 +1,9 @@
+# Copyright (C) 2021-2023 Rafinex S.à r.l., Jack S. Hale
+#
+# This file is part of FEniCSx-pctools (https://gitlab.com/rafinex-external-rifle/fenicsx-pctools)
+#
+# SPDX-License-Identifier:    LGPL-3.0-or-later
+
 __version__ = "0.7.0.dev0"
 
 from fenicsx_pctools import mat, pc
