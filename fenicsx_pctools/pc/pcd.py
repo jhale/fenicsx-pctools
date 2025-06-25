jhale/fenicsx-pctools

commit 9762865eca07c6a6ffe24745291c975621eb7d5b

Update to 0.9.0

fenicsx_pctools/pc/pcd.py
@@ -300,14 +300,15 @@ class PCDPC_vY(PCDPCBase):
             Keeping the identity term separated is important to get a stability with respect to
             the leading Stokes term.
         """
-        (z,) = self.get_work_vecs(x, 1)
+        (z, a) = self.get_work_vecs(x, 2)
 
+        # y = -(I + A_p^{-1} K_p) M_p^{-1} x,
         self.ksp_Mp.solve(x, y)  # y = M_p^{-1} x
-        self.Kp.mult(y, x)  # x = K_p y
-        self.bcs_applier(x)  # apply bcs to x
-        self.ksp_Ap.solve(x, z)  # z = A_p^{-1} x
+        self.Kp.mult(z, a)  # a = K_p z
+        self.bcs_applier(a)  # apply bcs to a
+        self.ksp_Ap.solve(a, z)  # z = A_p^{-1} a
         y.axpy(1.0, z)  # y = y + z
-        # FIXME: How is with the sign bussines?
+        # FIXME: How is with the sign business?
         y.scale(-1.0)  # y = -y
 
     def applyTranspose(self, pc, x, y):

.gitlab-ci.yml
@@ -1,6 +1,6 @@
 default:
   image:
-    name: dolfinx/dolfinx:v0.7.2
+    name: dolfinx/dolfinx:v0.9.0r1
     pull_policy: always # Not possible to use if-not-present on public runner.
 
 stages:

demo/demo_motivation.py
@@ -48,6 +48,7 @@ from petsc4py import PETSc
 import numpy as np
 
 import ufl
+from basix.ufl import element
 from dolfinx import fem
 from dolfinx.fem.petsc import (
     assemble_matrix_block,
@@ -61,10 +62,10 @@ from fenicsx_pctools.utils import vec_to_functions
 
 N = 12
 mesh = create_unit_cube(MPI.COMM_WORLD, N, N, N)
-elem = ufl.FiniteElement("Lagrange", mesh.ufl_cell(), 1)
+elem = element("Lagrange", mesh.basix_cell(), 1)
 
 # Create product space W = V x V x V
-W = [fem.FunctionSpace(mesh, elem) for _ in range(3)]
+W = [fem.functionspace(mesh, elem) for _ in range(3)]
 
 # Prepare test and trial functions on each subspace
 test_functions = [ufl.TestFunction(V) for V in W]
@@ -80,7 +81,7 @@ for i, (u_i, v_i) in enumerate(zip(test_functions, trial_functions)):
 f = [fem.Function(V) for V in W]
 
 for i, f_i in enumerate(f):
-    with f_i.vector.localForm() as f_i_loc:
+    with f_i.x.petsc_vec.localForm() as f_i_loc:
         f_i_loc.set(i + 1)  # set `f_i = i + 1` including ghost values
 
 L = [f_i * v_i * ufl.dx for f_i, v_i in zip(f, test_functions)]
@@ -130,7 +131,7 @@ def create_solver(A, prefix=None):
 
 def verify_solution(u, f):
     for u_i, f_i in zip(u, f):
-        with u_i.vector.localForm() as u_i_loc, f_i.vector.localForm() as f_i_loc:
+        with u_i.x.petsc_vec.localForm() as u_i_loc, f_i.x.petsc_vec.localForm() as f_i_loc:
             assert np.allclose(u_i_loc.array_r, f_i_loc.array_r, rtol=1e-6)
 
 

demo/mixed-poisson/demo_mixed-poisson.py
@@ -22,6 +22,7 @@ from petsc4py import PETSc
 
 import numpy as np
 
+from basix.ufl import element
 from dolfinx import fem, io, mesh
 from dolfinx.fem.petsc import assemble_matrix_block, assemble_vector_block, create_matrix_block
 from fenicsx_pctools.mat import create_splittable_matrix_block
@@ -29,12 +30,10 @@ from fenicsx_pctools.utils import vec_to_functions
 from ufl import (
     CellDiameter,
     FacetNormal,
-    FiniteElement,
     Measure,
     SpatialCoordinate,
     TestFunction,
     TrialFunction,
-    VectorElement,
     avg,
     div,
     exp,
@@ -53,10 +52,10 @@ domain = mesh.create_rectangle(
 )
 
 k = 1
-Q_el = FiniteElement("BDMCF", domain.ufl_cell(), k)
-P_el = FiniteElement("DG", domain.ufl_cell(), k - 1)
-Q = fem.FunctionSpace(domain, Q_el)
-P = fem.FunctionSpace(domain, P_el)
+Q_el = element("BDMCF", domain.basix_cell(), k)
+P_el = element("DG", domain.basix_cell(), k - 1)
+Q = fem.functionspace(domain, Q_el)
+P = fem.functionspace(domain, P_el)
 
 q = TrialFunction(Q)
 q_t = TestFunction(Q)
@@ -207,8 +206,8 @@ q_h = fem.Function(Q)
 p_h = fem.Function(P)
 vec_to_functions(u_h, [q_h, p_h])
 
-Q_out_el = VectorElement("Lagrange", domain.ufl_cell(), 1)
-Q_out = fem.FunctionSpace(domain, Q_out_el)
+Q_out_el = element("Lagrange", domain.basix_cell(), 1, shape=(domain.geometry.dim,))
+Q_out = fem.functionspace(domain, Q_out_el)
 
 q_h_out = fem.Function(Q_out)
 q_h_out.interpolate(q_h)

demo/navier-stokes-pcd/demo_navier-stokes-pcd.py
@@ -61,6 +61,7 @@ import numpy as np
 
 import dolfinx.fem.petsc  # noqa: F401
 import ufl
+from basix.ufl import element
 from dolfinx import fem
 from dolfinx.io import XDMFFile
 from dolfinx.io.gmshio import model_to_mesh
@@ -121,8 +122,8 @@ gmsh.finalize()
 # We use the classical Taylor-Hood element to compute the approximate solution.
 
 # +
-V_v = fem.FunctionSpace(mesh, ("P", 2, (mesh.geometry.dim,)))
-V_p = fem.FunctionSpace(mesh, ("P", 1))
+V_v = fem.functionspace(mesh, ("Lagrange", 2, (mesh.geometry.dim,)))
+V_p = fem.functionspace(mesh, ("Lagrange", 1))
 
 v = fem.Function(V_v, name="v")
 p = fem.Function(V_p, name="p")
@@ -220,7 +221,7 @@ class PDEProblem:
         x.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
         vec_to_functions(x, self.solution_vars)
 
-        fem.petsc.assemble_vector_block(F, self.F_dfx, self.J_dfx, self.bcs, x0=x, scale=-1.0)
+        fem.petsc.assemble_vector_block(F, self.F_dfx, self.J_dfx, self.bcs, x0=x, alpha=-1.0)
         F.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
 
     def J_block(self, snes, x, J, P):
@@ -359,7 +360,7 @@ PETSc.Sys.Print(
     f"Solver converged in {its_snes} nonlinear iterations"
     f" (with total number of {its_ksp} linear iterations)"
 )
-del solver
+solver.destroy()
 # -
 
 # As a last step, we save the results for visualisation.
@@ -368,8 +369,8 @@ del solver
 vec_to_functions(x0, pdeproblem.solution_vars)
 v_h, p_h = pdeproblem.solution_vars
 
-V_out_el = ufl.VectorElement("Lagrange", mesh.ufl_cell(), 1)
-V_out = fem.FunctionSpace(mesh, V_out_el)
+V_out_el = element("Lagrange", mesh.basix_cell(), 1, shape=(mesh.geometry.dim,))
+V_out = fem.functionspace(mesh, V_out_el)
 v_h_out = fem.Function(V_out, name="v")
 v_h_out.interpolate(v_h)
 

examples/rayleigh-benard-convection/problem_rayleigh_benard.py
@@ -4,12 +4,12 @@ from functools import cached_property
 import numpy as np
 
 import ufl
+from basix.ufl import element
 from dolfinx.fem import (
     Constant,
     Function,
-    FunctionSpace,
-    VectorFunctionSpace,
     dirichletbc,
+    functionspace,
     locate_dofs_topological,
 )
 from dolfinx.mesh import (
@@ -55,17 +55,19 @@ class Problem:
         mesh.topology.create_connectivity(2, 2)
         imap = mesh.topology.index_map(2)
         indices = np.arange(0, imap.size_local + imap.num_ghosts)
-        mesh_tags_facets = meshtags(mesh, 2, indices, np.zeros_like(indices, dtype=np.intc))
+        values = np.zeros_like(indices, dtype=np.int32)
         bndry_tag_map = {"left": 1, "right": 2, "rest": 3}
 
         bndry_mask = get_boundary_mask(mesh)
-        mesh_tags_facets.values[bndry_mask] = bndry_tag_map["rest"]
+        values[bndry_mask] = bndry_tag_map["rest"]
 
         left_facets = locate_entities(mesh, 2, lambda x: np.isclose(x[0], 0.0))
-        mesh_tags_facets.values[left_facets] = bndry_tag_map["left"]
+        values[left_facets] = bndry_tag_map["left"]
 
         right_facets = locate_entities(mesh, 2, lambda x: np.isclose(x[0], 1.0))
-        mesh_tags_facets.values[right_facets] = bndry_tag_map["right"]
+        values[right_facets] = bndry_tag_map["right"]
+
+        mesh_tags_facets = meshtags(mesh, 2, indices, values)
 
         return mesh, mesh_tags_facets, bndry_tag_map
 
@@ -99,12 +101,14 @@ class Problem:
     @cached_property
     def _mixed_space(self):
         mesh = self.domain_data[0]
-        family = "P" if mesh.ufl_cell() == ufl.tetrahedron else "Q"
+        family = "Lagrange"
 
         return OrderedDict(
-            v=VectorFunctionSpace(mesh, (family, 2), dim=3),
-            p=FunctionSpace(mesh, (family, 1)),
-            T=FunctionSpace(mesh, (family, 1)),
+            v=functionspace(
+                mesh, element(family, mesh.basix_cell(), 2, shape=(mesh.geometry.dim,))
+            ),
+            p=functionspace(mesh, (family, 1)),
+            T=functionspace(mesh, (family, 1)),
         )
 
     @property
@@ -119,7 +123,7 @@ class Problem:
     def num_dofs(self):
         num_dofs = 0
         for f in self.u:
-            num_dofs += f.vector.getSize()
+            num_dofs += f.x.petsc_vec.getSize()
 
         return num_dofs
 
@@ -206,7 +210,7 @@ class Problem:
         bcs.append(dirichletbc(v_walls, wallsdofsV_v))
 
         T_left = Function(V_T)
-        with T_left.vector.localForm() as T_local:
+        with T_left.x.petsc_vec.localForm() as T_local:
             T_local.set(1.0)
         bcs.append(dirichletbc(T_left, leftdofsV_T))
 

examples/rayleigh-benard-convection/test_rayleigh_benard.py
@@ -198,9 +198,9 @@ def test_rayleigh_benard(problem, pc_approach, timestamp, results_dir, request):
         def vec_to_functions(x, u):
             offset = 0
             for i in range(len(u)):
-                size_local = u[i].vector.getLocalSize()
-                u[i].vector.array[:] = x.array_r[offset : offset + size_local]
-                u[i].vector.ghostUpdate(
+                size_local = u[i].x.petsc_vec.getLocalSize()
+                u[i].x.petsc_vec.array[:] = x.array_r[offset : offset + size_local]
+                u[i].x.petsc_vec.ghostUpdate(
                     addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD
                 )
                 offset += size_local
@@ -223,7 +223,7 @@ def test_rayleigh_benard(problem, pc_approach, timestamp, results_dir, request):
             x.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
             self.vec_to_functions(x, self.solution_vars)
 
-            assemble_vector_block(F, self.F_dfx, self.J_dfx, self.bcs, x0=x, scale=-1.0)
+            assemble_vector_block(F, self.F_dfx, self.J_dfx, self.bcs, x0=x, alpha=-1.0)
             F.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
 
         def J_block(self, snes, x, J, P):

fenicsx_pctools/utils.py
@@ -4,39 +4,40 @@
 #
 # SPDX-License-Identifier:    LGPL-3.0-or-later
 
-import typing
 
 from petsc4py import PETSc
 
 import dolfinx
 
 
-def functions_to_vec(u: typing.List[dolfinx.fem.Function], x):
+def functions_to_vec(u: list[dolfinx.fem.Function], x):
     """Copies functions into block vector."""
     if x.getType() == "nest":
         for i, subvec in enumerate(x.getNestSubVecs()):
-            u[i].vector.copy(subvec)
+            u[i].x.petsc_vec.copy(subvec)
             subvec.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
     else:
         offset = 0
         for i in range(len(u)):
-            size_local = u[i].vector.getLocalSize()
+            size_local = u[i].x.petsc_vec.getLocalSize()
             with x.localForm() as loc:
                 loc.array[offset : offset + size_local] = u[i].vector.array_r
             offset += size_local
             x.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
 
 
-def vec_to_functions(x, u: typing.List[dolfinx.fem.Function]):
+def vec_to_functions(x, u: list[dolfinx.fem.Function]):
     """Copies block vector into functions."""
     if x.getType() == "nest":
         for i, subvec in enumerate(x.getNestSubVecs()):
-            subvec.copy(u[i].vector)
+            subvec.copy(u[i].x.petsc_vec)
             u[i].vector.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
     else:
         offset = 0
         for i in range(len(u)):
-            size_local = u[i].vector.getLocalSize()
-            u[i].vector.array[:] = x.array_r[offset : offset + size_local]
+            size_local = u[i].x.petsc_vec.getLocalSize()
+            u[i].x.petsc_vec.array[:] = x.array_r[offset : offset + size_local]
             offset += size_local
-            u[i].vector.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
+            u[i].x.petsc_vec.ghostUpdate(
+                addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD
+            )

commit 371cb3c6eb37f2b16aadbe92ed4c623ad4abfcaf

Fix bug introduced in 9762865e

fenicsx_pctools/pc/pcd.py
@@ -329,13 +329,13 @@ class PCDPC_vY(PCDPCBase):
             x: input vector
             y: output vector
         """
-        (z, a) = self.get_work_vecs(x, 2)
+        (z, w) = self.get_work_vecs(x, 2)
 
         # y = -(I + A_p^{-1} K_p) M_p^{-1} x,
         self.ksp_Mp.solve(x, y)  # y = M_p^{-1} x
-        self.Kp.mult(z, a)  # a = K_p z
-        self.bcs_applier(a)  # apply bcs to a
-        self.ksp_Ap.solve(a, z)  # z = A_p^{-1} a
+        self.Kp.mult(y, w)  # w = K_p y
+        self.bcs_applier(w)  # apply bcs to w
+        self.ksp_Ap.solve(w, z)  # z = A_p^{-1} w
         y.axpy(1.0, z)  # y = y + z
         # FIXME: How is with the sign business?
         y.scale(-1.0)  # y = -y